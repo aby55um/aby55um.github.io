6a4f069f1de13d0a9aaee2946d69d50a5efd98e0
PotatoJoe111 <dbu99r@protonmail.com>
2025-02-26 19:32:43 +0100

    Bare sidebar v0.01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">  
       </header>
+      <body>
+      <div id="sidebar">
+      <p>News</p>
+      <p>Resources</p>
+        {/* <ul>
+            <li>https://www.activecountermeasures.com </li>
+            <li>https://newandroidbook.com/TOC.html </li>
+            <li>https://guyinatuxedo.github.io/ </li>
+            <li>https://alice.climent-pommeret.red/posts/a-syscall-journey-in-the-windows-kernel/ </li>
+            <li>https://www.hoppersroppers.org/roadmap/training/pwning.html </li>
+            <li>https://github.com/SkyPenguinLabs/REplay </li>
+            <li>https://github.com/RPISEC/MBE </li>
+            <li>https://0x0539.net/ </li>
+            <li>https://github.com/angrave/SystemProgramming  </li>
+            <li>https://0xax.gitbooks.io/linux-insides/content/ </li>
+            <li>https://www.classcentral.com/report/best-reverse-engineering-courses/ </li>
+            <li>https://www.learnvulnerabilityresearch.com/resources  </li>
+            <li>https://crackmes.one/ </li>
+            <li>https://github.com/D4lj337/Awesome-Malware-and-Reversing  </li>
+            <li>https://godbolt.org/  </li>
+            <li>https://pwn.college/  </li>
+            <li>https://cryptohack.org/ </li>
+            <li>https://github.com/VirtualAlllocEx/DEFCON-31-Syscalls-Workshop  </li>
+            <li>https://github.com/aptnotes </li>
+            <li>https://malware-traffic-analysis.net/ </li>
+            <li>https://github.com/Th1ru-M/Windows-Threat-Hunting </li>
+            <li>https://vx-underground.org/ </li>
+            <li>https://class.malware.re/2021/01/24/malware-research-online.html  </li>
+            <li>https://github.com/Psmths/windows-forensic-artifacts  </li>
+            <li>https://mohammedalhumaid.com/wp-content/uploads/2022/01/windows-forensics-analysis-v-1.0-4.pdf  </li>
+            <li>https://github.com/Yara-Rules </li>
+        </ul> */}
+      <p>Ecosystem</p>
+      <p>Tools</p>
+      <p>Artifacts</p>
+      <p>Protocols</p>
+      <p>Malware</p>
+      </div>
+      </body>
     </div>
   );
 }
