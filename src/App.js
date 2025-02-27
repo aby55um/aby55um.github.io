0ae3f0f0696bc70acf164d2668cedc2d428bf439
PotatoJoe111 <dbu99r@protonmail.com>
2025-02-27 05:48:57 +0100

    Added a news for testing purposes

--- src/App.js
+++ src/App.js
@@ -6,43 +6,54 @@ function App() {
       <header className="App-header">  
       </header>
       <body>
-      <div id="sidebar">
-      <p>News</p>
-      <p>Resources</p>
-        {/* <ul>
-            <li>https://www.activecountermeasures.com </li>
-            <li>https://newandroidbook.com/TOC.html </li>
-            <li>https://guyinatuxedo.github.io/ </li>
-            <li>https://alice.climent-pommeret.red/posts/a-syscall-journey-in-the-windows-kernel/ </li>
-            <li>https://www.hoppersroppers.org/roadmap/training/pwning.html </li>
-            <li>https://github.com/SkyPenguinLabs/REplay </li>
-            <li>https://github.com/RPISEC/MBE </li>
-            <li>https://0x0539.net/ </li>
-            <li>https://github.com/angrave/SystemProgramming  </li>
-            <li>https://0xax.gitbooks.io/linux-insides/content/ </li>
-            <li>https://www.classcentral.com/report/best-reverse-engineering-courses/ </li>
-            <li>https://www.learnvulnerabilityresearch.com/resources  </li>
-            <li>https://crackmes.one/ </li>
-            <li>https://github.com/D4lj337/Awesome-Malware-and-Reversing  </li>
-            <li>https://godbolt.org/  </li>
-            <li>https://pwn.college/  </li>
-            <li>https://cryptohack.org/ </li>
-            <li>https://github.com/VirtualAlllocEx/DEFCON-31-Syscalls-Workshop  </li>
-            <li>https://github.com/aptnotes </li>
-            <li>https://malware-traffic-analysis.net/ </li>
-            <li>https://github.com/Th1ru-M/Windows-Threat-Hunting </li>
-            <li>https://vx-underground.org/ </li>
-            <li>https://class.malware.re/2021/01/24/malware-research-online.html  </li>
-            <li>https://github.com/Psmths/windows-forensic-artifacts  </li>
-            <li>https://mohammedalhumaid.com/wp-content/uploads/2022/01/windows-forensics-analysis-v-1.0-4.pdf  </li>
-            <li>https://github.com/Yara-Rules </li>
-        </ul> */}
-      <p>Ecosystem</p>
-      <p>Tools</p>
-      <p>Artifacts</p>
-      <p>Protocols</p>
-      <p>Malware</p>
-      </div>
+
+        <nav id="sidebar">
+          <p>News</p>
+          <p>Resources</p>
+            {/* <ul>
+                <li>https://www.activecountermeasures.com </li>
+                <li>https://newandroidbook.com/TOC.html </li>
+                <li>https://guyinatuxedo.github.io/ </li>
+                <li>https://alice.climent-pommeret.red/posts/a-syscall-journey-in-the-windows-kernel/ </li>
+                <li>https://www.hoppersroppers.org/roadmap/training/pwning.html </li>
+                <li>https://github.com/SkyPenguinLabs/REplay </li>
+                <li>https://github.com/RPISEC/MBE </li>
+                <li>https://0x0539.net/ </li>
+                <li>https://github.com/angrave/SystemProgramming  </li>
+                <li>https://0xax.gitbooks.io/linux-insides/content/ </li>
+                <li>https://www.classcentral.com/report/best-reverse-engineering-courses/ </li>
+                <li>https://www.learnvulnerabilityresearch.com/resources  </li>
+                <li>https://crackmes.one/ </li>
+                <li>https://github.com/D4lj337/Awesome-Malware-and-Reversing  </li>
+                <li>https://godbolt.org/  </li>
+                <li>https://pwn.college/  </li>
+                <li>https://cryptohack.org/ </li>
+                <li>https://github.com/VirtualAlllocEx/DEFCON-31-Syscalls-Workshop  </li>
+                <li>https://github.com/aptnotes </li>
+                <li>https://malware-traffic-analysis.net/ </li>
+                <li>https://github.com/Th1ru-M/Windows-Threat-Hunting </li>
+                <li>https://vx-underground.org/ </li>
+                <li>https://class.malware.re/2021/01/24/malware-research-online.html  </li>
+                <li>https://github.com/Psmths/windows-forensic-artifacts  </li>
+                <li>https://mohammedalhumaid.com/wp-content/uploads/2022/01/windows-forensics-analysis-v-1.0-4.pdf  </li>
+                <li>https://github.com/Yara-Rules </li>
+            </ul> */}
+          <p>Ecosystem</p>
+          <p>Tools</p>
+          <p>Artifacts</p>
+          <p>Protocols</p>
+          <p>Malware</p>
+        </nav>
+
+        <main>
+          <p className="post">
+            Leaked Black Basta Ransomware chat logs. 
+
+            Source: https://thehackernews.com/2025/02/leaked-black-basta-chat-logs-reveal.html
+
+            Feb 26, 2025
+          </p>
+        </main>
       </body>
     </div>
   );
